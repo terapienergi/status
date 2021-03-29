2232d01f30e0995d157973b6fa30f1a64a775031
Brian Sztamfater <briansztamfater@MacBook-Air-de-Brian.local>
2021-03-29 19:46:50 -0300

    Improved Start New Private Chat interface

--- src/status_im/ui/screens/add_new/new_chat/views.cljs
+++ src/status_im/ui/screens/add_new/new_chat/views.cljs
@@ -13,17 +13,20 @@
             [status-im.ui.components.topbar :as topbar]
             [status-im.utils.debounce :as debounce]
             [status-im.utils.utils :as utils]
-            [reagent.core :as reagent])
+            [reagent.core :as reagent]
+            [quo.react-native :as rn]
+            [clojure.string :as string]
+            [status-im.ui.components.invite.views :as invite]
+            [status-im.ethereum.ens :as ens]
+            [quo.platform :as platform])
   (:require-macros [status-im.utils.views :as views]))
 
 (defn- render-row [row]
-  (let [[first-name second-name] (multiaccounts/contact-two-names row false)]
+  (let [first-name (first (multiaccounts/contact-two-names row false))]
     [quo/list-item
      {:title    first-name
-      :subtitle second-name
       :icon     [chat-icon/contact-icon-contacts-tab
                  (multiaccounts/displayed-photo row)]
-      :chevron  true
       :on-press #(re-frame/dispatch [:chat.ui/start-chat
                                      (:public-key row)])}]))
 
@@ -61,9 +64,29 @@
     :yourself
     (i18n/label :t/can-not-add-yourself)))
 
+(defn filter-contacts [filter-text contacts]
+  (let [lower-filter-text (string/lower-case (str filter-text))
+        filter-fn         (fn [{:keys [name alias nickname]}]
+                            (or
+                             (string/includes? (string/lower-case (str name)) lower-filter-text)
+                             (string/includes? (string/lower-case (str alias)) lower-filter-text)
+                             (when nickname
+                               (string/includes? (string/lower-case (str nickname)) lower-filter-text))))]
+    (if filter-text
+      (filter filter-fn contacts)
+      contacts)))
+
+(defn is-valid-username? [username]
+  (let [is-chat-key? (and (string? username)
+                          (string/starts-with? username "0x")
+                          (= (count username) 132))
+        is-ens? (ens/valid-eth-name-prefix? username)]
+    (or is-chat-key? is-ens?)))
+
 (views/defview new-chat []
   (views/letsubs [contacts      [:contacts/active]
-                  {:keys [state ens-name public-key error]} [:contacts/new-identity]]
+                  {:keys [state ens-name public-key error]} [:contacts/new-identity]
+                  search-value (reagent/atom "")]
     [react/view {:style {:flex 1}}
      [topbar/topbar
       {:title  (i18n/label :t/new-chat)
@@ -76,11 +99,11 @@
                                                     :handler :contact/qr-code-scanned}])}]}]
      [react/view {:flex-direction :row
                   :padding        16}
-      [react/view {:flex          1
-                   :padding-right 16}
+      [react/view {:flex          1}
        [quo/text-input
         {:on-change-text
          #(do
+            (reset! search-value %)
             (re-frame/dispatch [:set-in [:contacts/new-identity :state] :searching])
             (debounce/debounce-and-dispatch [:new-chat/set-new-identity %] 600))
          :on-submit-editing
@@ -90,35 +113,57 @@
          :show-cancel         false
          :accessibility-label :enter-contact-code-input
          :auto-capitalize     :none
-         :return-key-type     :go}]]
-      [react/view {:justify-content :center
-                   :align-items     :center}
-       [input-icon state false nil]]]
-     [react/view {:min-height 30 :justify-content :flex-end}
-      [quo/text {:style {:margin-horizontal 16}
-                 :size  :small
-                 :align :center
-                 :color :secondary}
-       (cond (= state :error)
-             (get-validation-label error)
-
-             (= state :valid)
-             (str (if ens-name
-                    ens-name
-                    (gfycat/generate-gfy public-key))
-                  " • ")
-
-             :else "")
-       (when (= state :valid)
-         [quo/text {:monospace true
-                    :size      :inherit
-                    :color     :inherit}
-          (utils/get-shortened-address public-key)])]]
-     [list/flat-list {:data                      contacts
-                      :key-fn                    :address
-                      :render-fn                 render-row
-                      :enableEmptySections       true
-                      :keyboardShouldPersistTaps :always}]]))
+         :return-key-type     :go}]]]
+     [react/view (if (and
+                      (= (count contacts) 0)
+                      (= @search-value ""))
+                   {:flex 1}
+                   {:justify-content :flex-end})
+      (if (and
+           (= (count contacts) 0)
+           (= @search-value ""))
+        [react/view {:flex 1
+                     :align-items :center
+                     :padding-horizontal 58
+                     :padding-top 160}
+         [quo/text {:size  :base
+                    :align :center
+                    :color :secondary}
+          (i18n/label :t/you-dont-have-contacts-invite-friends)]
+         [invite/button]]
+        [list/flat-list {:data                      (filter-contacts @search-value contacts)
+                         :key-fn                    :address
+                         :render-fn                 render-row
+                         :enableEmptySections       true
+                         :keyboardShouldPersistTaps :always}])]
+     (when-not (= @search-value "")
+       [react/view
+        [quo/text {:style {:margin-horizontal 16
+                           :margin-vertical 14}
+                   :size  :base
+                   :align :left
+                   :color :secondary}
+         (i18n/label :t/non-contacts)]
+        (when (and (= state :searching)
+                   (is-valid-username? @search-value))
+          [rn/activity-indicator {:color colors/gray
+                                  :size  (if platform/android? :large :small)}])
+        (if (= state :valid)
+          [quo/list-item
+           {:title    ens-name
+            :subtitle-secondary public-key
+            :subtitle (gfycat/generate-gfy public-key)
+            :icon     [chat-icon/contact-icon-contacts-tab
+                       (multiaccounts/displayed-photo public-key)]
+            :on-press #(re-frame/dispatch [:chat.ui/start-chat public-key])}]
+          [quo/text {:style {:margin-horizontal 16}
+                     :size  :base
+                     :align :center
+                     :color :secondary}
+           (if (is-valid-username? @search-value)
+             (when (= state :error)
+               (get-validation-label error))
+             (i18n/label :t/invalid-username-or-key))])])]))
 
 (defn- nickname-input [entered-nickname]
   [quo/text-input
